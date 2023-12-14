662b06a543c47cae40926bbcc18a4a713bf09826
yune <peeper_o_o@naver.com>
2023-12-15 02:07:42 +0900

    Feat: IDE에서 프리뷰 기능 구현

--- ex_Color_displaymode/FeedVC.swift
+++ ex_Color_displaymode/FeedVC.swift
@@ -7,6 +7,28 @@
 
 import UIKit
 
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct VCPreview: PreviewProvider {
+        // Device 배열로 여러 개의 디바이스에 적용된 모습을 같이 확인할 수 있습니다.
+        // 저는 지금 3가지의 Device를 사용하고 있죠.
+    static var devices = ["iPhone SE", "iPhone 11 Pro Max", "iPhone 12"]
+
+    static var previews: some View {
+        ForEach(devices, id: \.self) { deviceName in
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "TabVC")
+                                // 익스텐션에서 만든 toPreview() 메서드를 사용하고 있죠!
+                .toPreview()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
+#endif
+
+
 class FeedVC: UIViewController{
     
     @IBOutlet weak var headerBar: Header_Spacelog!
@@ -34,6 +56,7 @@ class FeedVC: UIViewController{
     
     func setGroup(){
         let filters:[Switchable] = [filter1, filter2, filter3]
+        filter1.text = "llllll"
         filterGroup = RadioGroupingController(filters)
     }
     
@@ -123,3 +146,4 @@ extension FeedVC: UICollectionViewDelegateFlowLayout{
         
     }
 }
+
